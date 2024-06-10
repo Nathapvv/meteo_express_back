1402e45baf8d49962f3e754b8041ddab1922bf58
Reichmuth Nathanaël <nathanael.reich@gmail.com>
2024-06-10 09:07:10 +0200

    Initial commit

---
+++ src/main/java/org/meteo_express_back/MeteoExpressBackApplication.java
@@ -0,0 +1,13 @@
+package org.meteo_express_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoExpressBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoExpressBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/meteo_express_back/MeteoExpressBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meteo_express_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoExpressBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
