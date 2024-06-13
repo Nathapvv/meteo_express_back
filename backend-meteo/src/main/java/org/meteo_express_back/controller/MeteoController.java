bba4d81e102e9f029ded68ff5212bfa687fe68b8
Reichmuth Nathanaël <nathanael.reich@gmail.com>
2024-06-13 09:24:46 +0200

    refactor: modify name route

--- backend-meteo/src/main/java/org/meteo_express_back/controller/MeteoController.java
+++ backend-meteo/src/main/java/org/meteo_express_back/controller/MeteoController.java
@@ -4,16 +4,18 @@ import org.meteo_express_back.service.MeteoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/meteo")
 public class MeteoController {
 
     @Autowired
     private MeteoService meteoService;
 
-    @GetMapping("/meteo")
+    @GetMapping("/meteoday")
     public ResponseEntity<String> getMeteoData(
             @RequestParam(required = false) String ville,
             @RequestParam(required = false) Integer fenetre,
